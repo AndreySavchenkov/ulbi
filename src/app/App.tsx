d2ad86146b524eeeb895432eebaadcdc7474b10f
AndreySavchenkov <sannndvik16@gmail.com>
2023-08-23 12:31:49 +0200

    feat: added navigation, theme switcher, svgr

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,13 @@
 import './styles/index.scss'
-import { Link } from 'react-router-dom';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/NavBar';
 
 const App = () => {
-  const {theme,toggleTheme} = useTheme();
-
+  const {theme} = useTheme();
   return(
   <div className={classNames('app', {}, [theme])}>
-    <button onClick={toggleTheme}>TOGGLE</button>
     <Navbar />
     <AppRouter />
   </div>
