7dc08c85e32adbec84810ea27424648be7371f81
AndreySavchenkov <sannndvik16@gmail.com>
2023-08-24 07:52:33 +0200

    feat: added refresh webpack plugin

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -3,8 +3,9 @@ import type  {Configuration as DevServeConfiguration} from 'webpack-dev-server';
 
 export function buildDevServer (options: BuildOptions): DevServeConfiguration {
 return {
-port: options.port,
-open: true,
-historyApiFallback: true,
-}
+  port: options.port,
+  open: true,
+  historyApiFallback: true,
+  hot: true,
+  }
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack from 'webpack';
 import HTMLWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins ({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -14,6 +15,8 @@ export function buildPlugins ({paths, isDev}: BuildOptions): webpack.WebpackPlug
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    isDev && new webpack.HotModuleReplacementPlugin(),
+    isDev && new ReactRefreshWebpackPlugin(),
   ]
 }
